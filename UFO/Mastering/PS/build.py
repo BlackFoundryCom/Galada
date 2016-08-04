f095cfc621d53c7cad9b5417f9c056b5cb8bdf45
Jean-Baptiste Morizot <nompareille@gmail.com>
2016-08-04 12:47:33 +0200

    Fixing Assamese Glyphs plus Uukar and Uukaar composition

--- UFO/Mastering/PS/build.py
+++
@@ -1,7 +0,0 @@
-import subprocess, os
-
-cwd = os.getcwd()
-
-#subprocess.call(['makeotf', '-f', 'Galada.ufo', '-o', 'Galada.otf', '-ff', 'features', '-gf', 'glyphOrder', '-mf', 'menuname', '-r'], cwd=cwd)
-
-print os.split(cwd)
